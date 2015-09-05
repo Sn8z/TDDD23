f4ed877f04b380159013eb62edb034010115c295
Sn8z <marcus.sneitz@telia.com>
2015-09-05 16:25:18 +0200

    Added some more content to mainmenu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+	public void exitGame(){
+		Debug.LogError ("Quit Application");
+		Application.Quit ();
+	}
+}
