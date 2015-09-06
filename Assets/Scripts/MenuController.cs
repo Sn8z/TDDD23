a74788a01730fe7d4cd5335b95f29f69f5786440
Sn8z <marcus.sneitz@telia.com>
2015-09-06 14:02:56 +0200

    small changes to menu UI

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,6 +3,10 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class MenuController : MonoBehaviour {
+	public void loadLevel(int level){
+		Application.LoadLevel (level);
+	}
+
 	public void exitGame(){
 		Debug.LogError ("Quit Application");
 		Application.Quit ();
