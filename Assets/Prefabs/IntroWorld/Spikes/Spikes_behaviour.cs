62f09ac6e88256385f9924fbb4b5fb7e28ca6ac2
Sn8z <marcus.sneitz@telia.com>
2015-09-24 17:06:48 +0200

    fixed a mistake..

--- Assets/Prefabs/IntroWorld/Spikes/Spikes_behaviour.cs
+++ Assets/Prefabs/IntroWorld/Spikes/Spikes_behaviour.cs
@@ -15,7 +15,6 @@ public class Spikes_behaviour : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D col){
 		if(col.gameObject.tag == "Player"){
-			yield return new WaitForSeconds(3);
 			Application.LoadLevel(Application.loadedLevelName);
 		}
 	}
