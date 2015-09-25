ba2d26358dde04fcfe74e365bb9d0b7024ea9f80
Sn8z <marcus.sneitz@telia.com>
2015-09-25 12:45:14 +0200

    added mouselock prefab

---
+++ Assets/Scripts/LockMouse.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+}
