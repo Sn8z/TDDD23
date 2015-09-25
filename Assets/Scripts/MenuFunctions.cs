4027c018703b04ccb45bfbede0ade681bb4a92c1
Sn8z <marcus.sneitz@telia.com>
2015-09-25 12:16:11 +0200

    added simple level selection menu

---
+++ Assets/Scripts/MenuFunctions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuFunctions : MonoBehaviour {
+
+	public void loadLevel(int level){
+		Application.LoadLevel (level);
+	}
+
+	public void exitGame(){
+		Debug.LogError ("Quit Application");
+		Application.Quit ();
+	}
+}
