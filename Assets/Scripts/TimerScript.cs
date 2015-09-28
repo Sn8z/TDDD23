f0c81edebfa41df0964cec8b450f9261658d7328
Sn8z <marcus.sneitz@telia.com>
2015-09-28 13:37:52 +0200

    added simple timer

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerScript : MonoBehaviour {
+	float timer = 0f;
+	Text displayText;
+	// Use this for initialization
+	void Start () {
+		displayText = this.gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		displayText.text = timer.ToString("0.00") + " s";
+	}
+}
